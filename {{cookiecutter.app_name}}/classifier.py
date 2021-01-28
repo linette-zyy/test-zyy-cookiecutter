2ba6c3b026961334c9c04ed33ae91467227de711
linette-zyy <59192491+linette-zyy@users.noreply.github.com>
2021-01-28 13:20:13 +0800

    Update classifier.py

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -1,5 +1,5 @@
 2021.01.28
-kaixinkaixinkaixin
+kaixinkaixinkaixin  haoxiangmeitongbu
 '''
 This script performs the basic process for applying a machine learning
 algorithm to a dataset using Python libraries.
